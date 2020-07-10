3ddafd0767dd87287fffe0f1fe012f2f098e00c4
Justin Brandley <brandleyj@gmail.com>
2020-07-10 10:39:55 -0500

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,25 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    if length == 1:
+        return None
 
-    return None
+    elif length == 2:
+        num1 = weights[0]
+        num2 = weights[1]
+        if num1 + num2 == limit:
+            return [1, 0]
+        else:
+            return None
+        
+    else:
+        cache = {}
+        answer = []
+
+        for i in range(0, length):
+            cache[weights[i]] = i
+
+        for k, v in cache.items():
+            if limit - k in cache.keys():
+                answer.append(v)
+        return sorted(answer, reverse=True)
